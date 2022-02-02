4d7fdadec8dc4df52af1219dffd1d55898f9805b
alexvishnevskiy <mrflowy043@gmail.com>
2022-02-03 01:25:20 +0300

    Add tokenizer.pad

--- jigsaw/utils/tokenizer.py
+++ jigsaw/utils/tokenizer.py
@@ -1,6 +1,9 @@
 from nltk.tokenize import word_tokenize
+from typing import List, Dict, Any
 from collections import Counter
 import pickle
+import numpy as np
+import torch
 
 
 class Tokenizer:
@@ -40,14 +43,53 @@ class Tokenizer:
                     all_tokens.extend(tok)
                     
         c = Counter(all_tokens)
-        tokens_dict = {'<unk>': 0}
-        top_tokens = list(map(lambda x: x[0], c.most_common(n_tokens-1)))
-        tokens_dict.update(dict(zip(top_tokens, range(1, len(top_tokens)+1))))
+        tokens_dict = {'<unk>': 0, '<pad>': 1}
+        top_tokens = list(map(lambda x: x[0], c.most_common(n_tokens-2)))
+        tokens_dict.update(dict(zip(top_tokens, range(2, len(top_tokens)+2))))
         self.__tokens_dict = tokens_dict
 
     def save(self, path):
         with open(path, 'wb') as f:
             pickle.dump(self.__tokens_dict, f)
+
+    def pad(
+        self, 
+        features:List[Dict[str, Any]], 
+        padding=True, 
+        max_length=None, 
+        pad_to_multiple_of=None, 
+        return_tensors="pt"
+        ):
+        assert len(features) > 0
+        feat_keys = list(features[0].keys())
+
+        if padding == True or padding == 'longest':
+            max_length = max(len(feat[feat_keys[0]]) for feat in features)
+        if padding == 'max_length':
+            max_length = max_length if max_length is not None else 256
+        if padding == False or padding == 'do_not_pad':
+            max_length = 0
+
+        output_dict = dict(zip(feat_keys, [[] for _ in range(len(feat_keys))]))
+        for i in range(len(features)):
+            for k in feat_keys:
+                if k == 'target':
+                    output_dict[k].append(features[i][k])
+                    continue
+
+                len_feat_k = len(features[i][k])
+                max_length_sec = max_length if max_length != 0 else len(features[i][k])
+                if k == 'attention_mask':
+                    output_dict[k].append(features[i][k][:max_length_sec] + [0]*max(0, max_length-len_feat_k))
+                else:
+                    output_dict[k].append(features[i][k][:max_length_sec] + [1]*max(0, max_length-len_feat_k))
+                
+        for k, v in output_dict.items():
+            if return_tensors == 'pt':
+                output_dict[k] = torch.as_tensor(v)
+            else:
+                output_dict[k] = np.asarray(v)
+        return output_dict
 
     @classmethod
     def from_pretrained(cls, path):
